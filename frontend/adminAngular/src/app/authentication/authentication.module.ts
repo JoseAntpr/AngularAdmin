830e15a91957d6ee6ff90e56de1cbd94bcc6aae2
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-05-01 18:28:11 +0200

    authenticated state observable added

--- frontend/adminAngular/src/app/authentication/authentication.module.ts
+++ frontend/adminAngular/src/app/authentication/authentication.module.ts
@@ -1,20 +1,31 @@
 import { NgModule, ANALYZE_FOR_ENTRY_COMPONENTS, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthGoogleService } from './services/auth-google.service';
+import { TokenInterceptor } from './token-interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [],
-  providers: []
+  providers: [AuthGuard]
 })
 
 export class AuthenticationModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: AuthenticationModule,
-      providers: [AuthGoogleService]
+      providers: [
+        AuthGoogleService,
+        AuthGuard,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: TokenInterceptor,
+          multi: true
+        }
+      ]
     };
   }
 }
--- frontend/adminAngular/src/app/authentication/services/auth-google.service.ts
+++ frontend/adminAngular/src/app/authentication/services/auth-google.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AuthService, GoogleLoginProvider } from 'angularx-social-login';
+import { BehaviorSubject } from 'rxjs';
+
 
 @Injectable()
 export class AuthGoogleService {
+
+  public authSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public authObser = this.authSubject.asObservable();
 
   constructor(private authService: AuthService) { }
 
@@ -12,10 +17,12 @@ export class AuthGoogleService {
 
   signInWithGoogle(): void {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    this.authSubject.next(true);
   }
 
   signOut(): void {
     this.authService.signOut();
+    this.authSubject.next(false);
   }
 
 }
