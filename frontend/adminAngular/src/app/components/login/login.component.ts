776cac140eeb82820ac52d25cab381eb9590bff9
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-05-01 17:33:48 +0200

    login functionality added

--- frontend/adminAngular/src/app/components/login/login.component.ts
+++ frontend/adminAngular/src/app/components/login/login.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthGoogleService } from '../../authentication/services/auth-google.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription: Subscription;
+
+  constructor(public authService: AuthGoogleService, public router: Router) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authService.signInWithGoogle();
+    this.subscription = this.authService.authState.subscribe(token => {
+      if (token) {
+        console.log('Obteniendo nuestro tokenId', token);
+        localStorage.setItem('currentUser', JSON.stringify({ token: token.idToken}));
+        this.router.navigate(['/home']);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
