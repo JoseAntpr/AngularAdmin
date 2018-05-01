972a4ff0530a79ac187415db3bc823adb1c1c6d1
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-05-01 14:11:14 +0200

    Login component templating and styles added

--- frontend/adminAngular/src/app/material/material.module.ts
+++ frontend/adminAngular/src/app/material/material.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+
 // Material Modules
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatButtonModule
   ],
   exports: [
-
+    MatCardModule,
+    MatButtonModule
   ],
   declarations: []
 })
