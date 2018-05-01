9fb9c74caa3724208df11b6e1d01a4ab5523e42f
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-05-01 13:58:53 +0200

    Login components and material module added

--- frontend/adminAngular/src/app/app.module.ts
+++ frontend/adminAngular/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+// Modules
+import { MaterialModule } from './material/material.module';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
